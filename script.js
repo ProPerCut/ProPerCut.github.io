d01f26c55442c3eaa5b5123d0522049a764f4536
ProPerCut <banglashiyal@gmail.com>
2026-01-05 03:01:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,18 @@ gsap.utils.toArray(".process-step").forEach((step, i) => {
     }
   });
 });
+// CONTACT PAGE FADE IN
+gsap.from(".contact-hero *", {
+  y: 40,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1
+});
+
+gsap.from(".form-step", {
+  y: 40,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.12,
+  delay: 0.4
+});
