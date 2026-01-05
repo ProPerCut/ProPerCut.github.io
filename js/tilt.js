da09127c60c9357d83bb75a5dec7f3c16d759164
ProPerCut <banglashiyal@gmail.com>
2026-01-05 05:34:33 -0600

    Update tilt.js

--- js/tilt.js
+++ js/tilt.js
@@ -1,1 +1,34 @@
+/* =========================
+   3D TILT EFFECT
+========================= */
+
+document.querySelectorAll(".tilt").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = -(y - centerY) / 12;
+    const rotateY = (x - centerX) / 12;
+
+    card.style.transform = `
+      perspective(800px)
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      scale(1.05)
+    `;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = `
+      perspective(800px)
+      rotateX(0)
+      rotateY(0)
+      scale(1)
+    `;
+  });
+});
 
