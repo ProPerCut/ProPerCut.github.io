14f10dd8558c79eb5c46336f696d189778043147
ProPerCut <banglashiyal@gmail.com>
2026-01-07 07:52:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const cursor=document.getElementById("cursor")
-const scissors=document.getElementById("scissors")
-const cutSound=new Audio("assets/sound/cut.mp3")
+const leftSound=new Audio("assets/sound/cut.mp3")
+const rightSound=new Audio("assets/sound/right.mp3")
 
 document.addEventListener("mousemove",e=>{
   gsap.to(cursor,{x:e.clientX,y:e.clientY,duration:.1})
 })
 
-document.addEventListener("mousedown",()=>{
-  cutSound.currentTime=0
-  cutSound.play()
-  gsap.fromTo(scissors,{scale:1},{scale:.6,repeat:1,yoyo:true})
+document.addEventListener("mousedown",e=>{
+  if(e.button===0){leftSound.play()}
+  if(e.button===2){rightSound.play()}
 })
 
-const skills=["CRIME DOCUMENTARY","FACELESS AUTOMATION","SCIENCE EXPLAINER","MYSTERY STORIES"]
+/* typing */
+const text="DOCUMENTARY & FACELESS VIDEO EDITOR"
 let i=0
 setInterval(()=>{
-  document.getElementById("skillText").innerText=skills[i++%skills.length]
-},2500)
-
+ document.querySelector(".typing").innerText=text.slice(0,i++)
+ if(i>text.length)i=0
+},120)
