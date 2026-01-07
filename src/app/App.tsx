1b13fd0a40823d0b0ab36ad0b9a195f0c9dc3ea0
ProPerCut <banglashiyal@gmail.com>
2026-01-07 03:41:58 -0600

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,1 +1,24 @@
+import { Header } from "./components/Header";
+import { HeroPremium } from "./components/HeroPremium";
+import { PortfolioPremium } from "./components/PortfolioPremium";
+import { ChannelExperience } from "./components/ChannelExperience";
+import { AboutPremium } from "./components/AboutPremium";
+import { ContactPremium } from "./components/ContactPremium";
+import { FooterProperCut } from "./components/FooterProperCut";
+import { CustomCursorPremium } from "./components/CustomCursorPremium";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white overflow-x-hidden antialiased">
+      <CustomCursorPremium />
+      <Header />
+      <HeroPremium />
+      <PortfolioPremium />
+      <ChannelExperience />
+      <AboutPremium />
+      <ContactPremium />
+      <FooterProperCut />
+    </div>
+  );
+}
 
